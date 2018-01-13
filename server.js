865b40ff0bf7a46a8bbb7771f18931129594b640
Nichipedia <atreayou@gmail.com>
2018-01-13 15:53:21 -0600

    finished phase1

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ var express = require('express');
 var chalk = require('chalk');
 var app = express();
 var router = express.Router();
+var fs = require('fs');
+var emrBank;
+
+fs.readFile('./emr.json', 'utf-8', (err, data) => {
+    if(err) {
+        console.log(chalk.red(err));
+    } else {
+        emrBank = JSON.parse(data);
+    }
+});
 
 router.get('/test', function(req, res) {
     res.status(200).send('Hello world');
+});
+
+router.get('/emr', (req, res) => {
+    res.json(emrBank);
+});
+
+router.get('/emr/:id', (req, res) => {
+    res.json(emrBank[req.params.id]);
 });
 
 app.use('/api', router);
